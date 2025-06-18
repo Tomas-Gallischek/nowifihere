5244db7588b36dd89e237b69b3f64c94b2d170f6
Tomas Gallischek <t.gall@seznam.cz>
2025-06-18 17:49:16 +0200

    Oprava detailu hráčů

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.adminpage, name='adminpage'),
     path('statistiky_avataru', views.statistiky),
-    path('detail_hracu', views.detail_vsech_hracu),
+    path('detail_hracu', views.detail_vsech_hracu, name='detail_hracu'),
 ]
